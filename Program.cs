98559b2d563643adba6f6cbd4f831b0a0ac24218
Caleb Miller <caleb.miller734@outlook.com>
2018-02-11 23:10:51 -0500

    fleshed out structure of project

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 namespace explorica
 {
-    class Program
+    public class Program
     {
         private static int minStar = 0;
         private static int[] minStarArr = new int[minStar];
@@ -11,33 +11,39 @@ namespace explorica
         private static string spaceString = "";
         private static string starString = "*";
         private static string finalString = "";
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Starting program...");
+            
+            Program prog = new Program();
+            
+            Program2 prog2 = new Program2(); 
+            if(prog2 != null)
+            {
+                
+            }
         }
-        public void function()
+        public void MyFunction()
         {
             foreach (int i in linesArr)
             {
                 foreach (int j in linesArr)
                 {
                     spaceString += " ";
-                }
+                }// linesArr loop
                 if (i != 0)
                 {
                     foreach (int a in minStarArr)
                     {
                         starString += "**";
-                    }
-                }
+                    }// minStarArr loop
+                }// 1 != 0 check
                 finalString = spaceString + starString + spaceString;
                 Console.Write(finalString);
                 numLines--;
                 minStar++;
             }
         }
-        Program prog = new Program();
-        Program.function();
     }
 }
 
---
+++ q2.cs
@@ -0,0 +1,45 @@
+using System;
+
+public class Program2
+{
+    public void doStuff() 
+    {
+        //Console.WriteLine("doing stuff...");
+    }
+//     private static int minStar = 0;
+//     private static int[] minStarArr = new int[minStar];
+//     private static int numLines = 6;
+//     private static int[] linesArr = new int[numLines];
+//     private static string spaceString = "";
+//     private static string starString = "*";
+//     private static string finalString = "";
+//   /*  static void Main(string[] args)
+//     {
+//         Console.WriteLine("Starting Program2...");
+//     }*/
+
+
+//     void function()
+//     {
+//         foreach (int i in linesArr)
+//         {
+//             foreach (int j in linesArr)
+//             {
+//                 spaceString += " ";
+//             }
+//             if (i != 0)
+//             {
+//                 foreach (int a in minStarArr)
+//                 {
+//                     starString += "**";
+//                 }
+//             }
+//             finalString = spaceString + starString + spaceString;
+//             Console.Write(finalString);
+//             numLines--;
+//             minStar++;
+//         }
+//     }
+}
+
+
